8bd091c5bb3f459bacdc2c91912b23dd6a8ca6ec
Zdeněk Gromnica <futuremillennium@gmail.com>
2016-03-15 23:08:40 +0100

    * build all *.o instead of specific files

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ i686-elf-as Alopos/boot.s -o build/boot.o
 i686-elf-gcc -c Alopos/kernel.c -o build/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
 cd build
-i686-elf-gcc -T linker.ld -o ../bin/alopos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+i686-elf-gcc -T linker.ld -o ../bin/alopos.bin -ffreestanding -O2 -nostdlib *.o -lgcc
 cd ..
 
 REM grub-mkrescue -o iso/alopos.iso bin
