dfa75f240e312b34a110f99b6a4aedfb864833f2
Zdeněk Gromnica <futuremillennium@gmail.com>
2016-03-19 23:05:01 +0100

    + Number2String + Echo_Terminal_VGA %i (signed int) and %x (hex) support * clear screen and start at the top + VGA terminal colours test + Alopos OS name and version numbers constants

--- Alopos/kernel.c
+++ Alopos/kernel.c
@@ -51,22 +51,14 @@ Index Length_String(const char* input) {
 
 static const Index COLS_MAX_VGA = 80;
 static const Index ROWS_MAX_VGA = 25;
+static const char* OS_NAME = "Alopos";
+static const Byte OS_VERSION_MAJOR = 0;
+static const Byte OS_VERSION_MINOR = 1;
 
 Index rowCurrentTerminal_VGA;
 Index colCurrentTerminal_VGA;
 Byte colorCurrentTerminal_VGA;
 volatile Byte2* bufferTerminal_VGA;
-
-void Init_Terminal_VGA() {
-	rowCurrentTerminal_VGA = ROWS_MAX_VGA - 1;
-	colCurrentTerminal_VGA = 0;
-
-	colorCurrentTerminal_VGA = ColorMake_VGA(COLOR_LIGHT_GREY, COLOR_BLACK);
-
-	// TODO monochrome monitors at 0xB0000?
-	// TODO video address const
-	bufferTerminal_VGA = (Byte2*)0xB8000;
-}
 
 void ColorSet_Terminal_VGA(Byte color) {
 	colorCurrentTerminal_VGA = color;
@@ -101,6 +93,42 @@ void PutChar_Terminal_VGA(char character) {
 	}
 }
 
+void Number2String(int number, char* buffer, char base) {
+	char *tmpbuffer = buffer;
+	char *p1, *p2;
+	unsigned long unsignedNumber = number;
+	int divisor = 10;
+
+	// %i is assumed to be signed, otherwise unsigned
+	if (base == 'i' && number < 0) {
+		*tmpbuffer++ = '-';
+		buffer++;
+		unsignedNumber = -number;
+	} else if (base == 'x')
+		divisor = 16;
+
+	// divide by divisor until 0
+	do {
+		int remainder = unsignedNumber % divisor;
+
+		*tmpbuffer++ = (remainder < 10) ? (remainder + '0') : (remainder + 'A' - 10);
+	} while (unsignedNumber /= divisor);
+
+	// 0-terminated string
+	*tmpbuffer = 0;
+
+	/* Reverse BUF. */
+	p1 = buffer;
+	p2 = tmpbuffer - 1;
+	while (p1 < p2) {
+		char tmp = *p1;
+		*p1 = *p2;
+		*p2 = tmp;
+		p1++;
+		p2--;
+	}
+}
+
 // Echo("Foo");
 // Echo("String: %s", "foo");
 // Echo("String: %s%", "foo", "\nAnd another: %s", "bar");
@@ -123,6 +151,13 @@ void Echo_Terminal_VGA(const char* text, ...) {
 
 				case 'i':
 				case 'x':
+				{
+					char buffer[17];
+					args++;
+					Number2String(*(int*)args, buffer, c);
+					tmp = buffer;
+					goto echoString;
+				}
 					// TODO itoa
 					break;
 
@@ -147,6 +182,23 @@ void Echo_Terminal_VGA(const char* text, ...) {
 	}
 }
 
+void Init_Terminal_VGA() {
+	colorCurrentTerminal_VGA = ColorMake_VGA(COLOR_LIGHT_GREY, COLOR_BLACK);
+
+	rowCurrentTerminal_VGA = 0;
+	colCurrentTerminal_VGA = 0;
+
+	// TODO monochrome monitors at 0xB0000?
+	// TODO video address const
+	bufferTerminal_VGA = (Byte2*)0xB8000;
+
+	for (Byte y = 0; y < ROWS_MAX_VGA; y++) {
+		for (Byte x = 0; x < COLS_MAX_VGA; x++) {
+			EntryPut_Terminal_VGA(0, colorCurrentTerminal_VGA, x, y);
+		}
+	}
+}
+
 #if defined(__cplusplus)
 extern "C" // Use C linkage for main_kernel.
 #endif
@@ -154,17 +206,37 @@ void Main_Kernel() {
 
 	Init_Terminal_VGA();
 	
-	Echo_Terminal_VGA("Alopos says hello!\nCharset test:\n");
+	ColorSet_Terminal_VGA(ColorMake_VGA(COLOR_WHITE, COLOR_BLACK));
+	Echo_Terminal_VGA("%s v%i.%i\n", OS_NAME, OS_VERSION_MAJOR, OS_VERSION_MINOR);
 
+	ColorSet_Terminal_VGA(ColorMake_VGA(COLOR_LIGHT_GREY, COLOR_BLACK));
 	// test charset
-	char i = 0;
+	Echo_Terminal_VGA("Charset test:\n");
+	{
+		char i = 0;
 
-	do {
-		EntryPut_Terminal_VGA(i, colorCurrentTerminal_VGA, colCurrentTerminal_VGA, rowCurrentTerminal_VGA);
-		if ((colCurrentTerminal_VGA++) == COLS_MAX_VGA) {
-			NewLine_Terminal_VGA();
+		do {
+			EntryPut_Terminal_VGA(i, colorCurrentTerminal_VGA, colCurrentTerminal_VGA, rowCurrentTerminal_VGA);
+			if ((colCurrentTerminal_VGA++) == COLS_MAX_VGA) {
+				NewLine_Terminal_VGA();
+			}
+			i++;
+		} while (i != 0);
+	}
+
+	Echo_Terminal_VGA("\nColours test:\n");
+
+	{
+		for (Byte i = 0; i < 16; i++) {
+			for (Byte j = 0; j < 16; j++) {
+				EntryPut_Terminal_VGA('0' + j, ColorMake_VGA(j, i), colCurrentTerminal_VGA, rowCurrentTerminal_VGA);
+				colCurrentTerminal_VGA++;
+			}
+			if (i % 4 == 3) {
+				NewLine_Terminal_VGA();
+			}
 		}
-		i++;
-	} while (i != 0);
+	}
+
 
 }
